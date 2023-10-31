171ee8d5bf79c2d5def379c330efdc338bf73b98
Sebastian Szypulski <sszypulski.kess@gmail.com>
2023-10-31 22:29:43 +0100

    Update server.go

--- server.go
+++ server.go
@@ -12,7 +12,7 @@ func hello(w http.ResponseWriter, req *http.Request) {
 
 func home(w http.ResponseWriter, req *http.Request) {
 
-	fmt.Fprintf(w, "M.A.R.I.A.N - DWSC\n")
+    fmt.Fprintf(w, "M.A.R.I.A.N - DWSC\n")
     fmt.Fprintf(w, "Manualy Activated Recovery of Infrastructue, Applications and Networking - Debug and Web Server Container\n")
 }
 
@@ -27,9 +27,9 @@ func headers(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 
-	fmt.Printf("M.A.R.I.A.N - DWSC\n")
+    fmt.Printf("M.A.R.I.A.N - DWSC\n")
     fmt.Printf("Manualy Activated Recovery of Infrastructue, Applications and Networking - Debug and Web Server Container\n")
-	http.HandleFunc("/", home)
+    http.HandleFunc("/", home)
     http.HandleFunc("/hello", hello)
     http.HandleFunc("/headers", headers)
 
